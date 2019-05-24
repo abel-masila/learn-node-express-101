34bc3f0cb8ea2df3b617c6dc772089a9cad2b0d7
Masila <abelmasila@gmail.com>
2019-05-24 13:27:05 +0300

    added joi validate

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const Joi = require("@hapi/joi");
+
 const app = express();
+
+app.use(express.json());
 
 const courses = [
   { id: 1, name: "NodeJS" },
@@ -10,10 +14,11 @@ const courses = [
 app.get("/", (req, res) => {
   res.send("Hello Home");
 });
-
+//all courses
 app.get("/api/courses", (req, res) => {
   res.send(courses);
 });
+//single course
 app.get("/api/courses/:id", (req, res) => {
   const id = req.params.id;
   //filter courses array by id
@@ -21,6 +26,26 @@ app.get("/api/courses/:id", (req, res) => {
   if (!course)
     res.status(404).send("The course with the given ID was not found");
   res.send(course);
+});
+//post request
+app.post("/api/courses", (req, res) => {
+  const Schema = {
+    name: Joi.string()
+      .min(3)
+      .required()
+  };
+  const result = Joi.validate(req.body, Schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name
+  };
+
+  courses.push(course);
+  res.status(201).send(course);
 });
 //listen to port
 const port = process.env.PORT || 3000;
